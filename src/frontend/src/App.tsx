fecb1a39e3867ce36d1602dc11cc3c35794b8f8c
Kate Min <mink9@mcmaster.ca>
2025-01-26 15:22:58 -0500

    feat(frontend): add new sidebar and layout

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,16 +1,22 @@
 import "./App.css";
-import Sidebar from "./components/sidebars/Sidebar";
+import Layout from "./components/Layout";
 import TableView from "./pages/TableView";
 import Upload from "./pages/Upload";
+import Dashboard from "./pages/Dashboard";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className=" bg-gradient-to-b from-blue-50 to-blue-100">
-      <div className="flex bg-gradient-to-b from-blue-50 to-blue-100">
-        <Sidebar />
-      </div>
-      <Upload />
-      <TableView />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/table" element={<TableView />} />
+          {/* CHANGE TO GRAPH PAGE */}
+          <Route path="/graphs" element={<Dashboard />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
