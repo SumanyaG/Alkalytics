1d7ecabb94eb0d446034e51be8723719d8e51c18
Kate Min <mink9@mcmaster.ca>
2025-01-31 21:21:29 -0500

    add react-cookie library

--- src/frontend/src/utils/apolloClient.ts
+++ src/frontend/src/utils/apolloClient.ts
@@ -4,6 +4,7 @@ import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
 const client = new ApolloClient({
   link: createUploadLink({
     uri: "http://localhost:8000/graphql",
+    credentials: "include",
   }),
   cache: new InMemoryCache(),
 });
