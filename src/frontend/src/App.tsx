3c7b029357432111c04789a7d9f78aec1e15f6d6
Kate Min <mink9@mcmaster.ca>
2025-01-31 21:28:53 -0500

    fix(frontend): update main app routes and layout components

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,22 +1,31 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/authContext";
 import Layout from "./components/Layout";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 import TableView from "./pages/TableView";
 import Upload from "./pages/Upload";
 import Dashboard from "./pages/Dashboard";
-import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className=" bg-gradient-to-b from-blue-50 to-blue-100">
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/table" element={<TableView />} />
-          {/* CHANGE TO GRAPH PAGE */}
-          <Route path="/graphs" element={<Dashboard />} />
-        </Route>
-      </Routes>
+    <div className="bg-gradient-to-b from-blue-50 to-blue-100">
+      <AuthProvider>
+        <Routes>
+          <Route path="*" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route element={<ProtectedRoute><Layout /></ProtectedRoute>}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/upload" element={<Upload />} />
+            <Route path="/table" element={<TableView />} />
+            {/* CHANGE TO GRAPH PAGE */}
+            <Route path="/graphs" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
