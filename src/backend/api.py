fafd232722c52afa8e00e70d6e8034155a862adf
Jenniferye25 <jennifer.ye3@gmail.com>
2025-02-03 00:23:46 -0500

    feat(backend): Adds get attribute endpoint

--- src/backend/api.py
+++ src/backend/api.py
@@ -66,167 +66,188 @@ async def upload(payload: FilesPayload):
     tempExperimentFiles = []
     tempDataFiles = []
 
+#     try:
+
+#         def decodeAndSave(filePayload: FilePayload) -> str:
+#             try:
+#                 file_content = base64.b64decode(filePayload.content)
+#             except Exception as decode_error:
+#                 raise HTTPException(
+#                     status_code=400,
+#                     detail=f"Invalid base64 content for file: {
+#                         filePayload.filename}",
+#                 ) from decode_error
+
+#             sanitized_filename = sanitizeFilename(filePayload.filename)
+#             tempFilePath = os.path.join(os.getcwd(), sanitized_filename)
+
+#             with open(tempFilePath, "wb") as temp_file:
+#                 temp_file.write(file_content)
+
+#             return tempFilePath
+
+#         for file in payload.experimentFiles:
+#             tempExperimentFiles.append(decodeAndSave(file))
+
+#         for file in payload.dataFiles:
+#             tempDataFiles.append(decodeAndSave(file))
+
+#         if not tempExperimentFiles and not tempDataFiles:
+#             raise HTTPException(
+#                 status_code=400, detail="No valid files provided."
+#             )
+
+#         mongoUri = os.getenv("CONNECTION_STRING")
+#         dbName = "alkalyticsDB"
+#         migrationService = MigrationService(mongoUri, dbName)
+
+#         try:
+#             ambiguous_data = await migrationService.migrate(
+#                 experimentFilePaths=tempExperimentFiles,
+#                 dataFilePaths=tempDataFiles,
+#             )
+#         finally:
+#             await migrationService.closeConnection()
+
+#         file_map = {file.filename: file for file in payload.dataFiles}
+
+#         for data in ambiguous_data:
+#             if data["dataId"] in file_map:
+#                 data["dataFile"] = file_map[data["dataId"]]
+
+#         # Clean up temporary files
+#         for tempFile in tempExperimentFiles:
+#             os.remove(tempFile)
+#         for tempFile in tempDataFiles:
+#             os.remove(tempFile)
+
+#         return {
+#             "status": "success",
+#             "message": "Files processed successfully.",
+#             "ambiguousData": ambiguous_data,
+#         }
+
+#     except HTTPException as e:
+#         raise e
+#     except Exception as e:
+#         # Clean up temporary experiment files
+#         for tempFile in tempExperimentFiles:
+#             if os.path.exists(tempFile):
+#                 os.remove(tempFile)
+
+#         # Clean up temporary data files
+#         for tempFile in tempDataFiles:
+#             if os.path.exists(tempFile):
+#                 os.remove(tempFile)
+
+#         raise HTTPException(
+#             status_code=500, detail=f"Error processing files: {str(e)}"
+#         )
+
+
+# class LinkedDataPayload(BaseModel):
+#     filename: str
+#     mimetype: str
+#     content: str
+#     linkedId: str
+
+
+# class ManualUploadPayload(BaseModel):
+#     linkedData: list[LinkedDataPayload]
+
+
+# @app.post("/manual-upload")
+# async def manualUpload(payload: ManualUploadPayload):
+#     """
+#     Processes uploaded files for experiment and data categories.
+#     Decodes base64 content, saves them temporarily, uses
+#     MigrationService to handle the files, and cleans up resources.
+#     """
+#     tempLinkedDataFiles = []
+
+#     try:
+#         def decodeAndSave(filePayload: LinkedDataPayload) -> dict:
+#             try:
+#                 file_content = base64.b64decode(filePayload.content)
+#             except Exception as decode_error:
+#                 raise HTTPException(
+#                     status_code=400,
+#                     detail=f"Invalid base64 content for file: {
+#                         filePayload.filename}",
+#                 ) from decode_error
+
+#             sanitized_filename = sanitizeFilename(filePayload.filename)
+#             tempFilePath = os.path.join(os.getcwd(), sanitized_filename)
+
+#             with open(tempFilePath, "wb") as temp_file:
+#                 temp_file.write(file_content)
+
+#             return {"path": tempFilePath, "linkedId": filePayload.linkedId}
+
+#         for file in payload.linkedData:
+#             tempLinkedDataFiles.append(decodeAndSave(file))
+
+#         mongoUri = os.getenv("CONNECTION_STRING")
+#         dbName = "alkalyticsDB"
+#         migrationService = MigrationService(mongoUri, dbName)
+
+#         try:
+#             for tempData in tempLinkedDataFiles:
+#                 dataDf = pd.read_excel(tempData["path"], sheet_name=0)
+#                 dataDf = migrationService.cleanData(dataDf)
+
+#                 records = await migrationService.linkData(
+#                     dataDf, tempData["linkedId"]
+#                 )
+
+#                 if records:
+#                     await migrationService.dataSheetsCollection.insert_many(
+#                         records
+#                     )
+#         finally:
+#             await migrationService.closeConnection()
+
+#         for tempFile in tempLinkedDataFiles:
+#             if os.path.exists(tempFile["path"]):
+#                 os.remove(tempFile["path"])
+
+#         return {
+#             "status": "success",
+#             "message": "Files processed successfully.",
+#         }
+
+#     except HTTPException as e:
+#         raise e
+#     except Exception as e:
+#         for tempFile in tempLinkedDataFiles:
+#             if os.path.exists(tempFile):
+#                 os.remove(tempFile)
+
+#         raise HTTPException(
+#             status_code=500, detail=f"Error processing files: {str(e)}"
+#         )
+
+class DataVisualizeRequest(BaseModel):
+    pass
+@app.get("/experiments/getAttrs")
+async def getExperimentAttrs():
+    connection = getConnection("experiments")
+    collection, client = connection["collection"], connection["client"]
+
     try:
-
-        def decodeAndSave(filePayload: FilePayload) -> str:
-            try:
-                file_content = base64.b64decode(filePayload.content)
-            except Exception as decode_error:
-                raise HTTPException(
-                    status_code=400,
-                    detail=f"Invalid base64 content for file: {
-                        filePayload.filename}",
-                ) from decode_error
-
-            sanitized_filename = sanitizeFilename(filePayload.filename)
-            tempFilePath = os.path.join(os.getcwd(), sanitized_filename)
-
-            with open(tempFilePath, "wb") as temp_file:
-                temp_file.write(file_content)
-
-            return tempFilePath
-
-        for file in payload.experimentFiles:
-            tempExperimentFiles.append(decodeAndSave(file))
-
-        for file in payload.dataFiles:
-            tempDataFiles.append(decodeAndSave(file))
-
-        if not tempExperimentFiles and not tempDataFiles:
-            raise HTTPException(
-                status_code=400, detail="No valid files provided."
-            )
-
-        mongoUri = os.getenv("CONNECTION_STRING")
-        dbName = "alkalyticsDB"
-        migrationService = MigrationService(mongoUri, dbName)
-
-        try:
-            ambiguous_data = await migrationService.migrate(
-                experimentFilePaths=tempExperimentFiles,
-                dataFilePaths=tempDataFiles,
-            )
-        finally:
-            await migrationService.closeConnection()
-
-        file_map = {file.filename: file for file in payload.dataFiles}
-
-        for data in ambiguous_data:
-            if data["dataId"] in file_map:
-                data["dataFile"] = file_map[data["dataId"]]
-
-        # Clean up temporary files
-        for tempFile in tempExperimentFiles:
-            os.remove(tempFile)
-        for tempFile in tempDataFiles:
-            os.remove(tempFile)
-
-        return {
-            "status": "success",
-            "message": "Files processed successfully.",
-            "ambiguousData": ambiguous_data,
-        }
-
-    except HTTPException as e:
-        raise e
+        data = collection.find_one()
+        datalist = list(data)
+        if datalist:
+            return {"status": "success", "data": data}
+        else:
+            return {"status": "error", "message": "No documents found in the collection."}
+        
     except Exception as e:
-        # Clean up temporary experiment files
-        for tempFile in tempExperimentFiles:
-            if os.path.exists(tempFile):
-                os.remove(tempFile)
+        print("errored out here")
+        return {"status": "error", "message": str(e)}
 
-        # Clean up temporary data files
-        for tempFile in tempDataFiles:
-            if os.path.exists(tempFile):
-                os.remove(tempFile)
-
-        raise HTTPException(
-            status_code=500, detail=f"Error processing files: {str(e)}"
-        )
-
-
-class LinkedDataPayload(BaseModel):
-    filename: str
-    mimetype: str
-    content: str
-    linkedId: str
-
-
-class ManualUploadPayload(BaseModel):
-    linkedData: list[LinkedDataPayload]
-
-
-@app.post("/manual-upload")
-async def manualUpload(payload: ManualUploadPayload):
-    """
-    Processes uploaded files for experiment and data categories.
-    Decodes base64 content, saves them temporarily, uses
-    MigrationService to handle the files, and cleans up resources.
-    """
-    tempLinkedDataFiles = []
-
-    try:
-        def decodeAndSave(filePayload: LinkedDataPayload) -> dict:
-            try:
-                file_content = base64.b64decode(filePayload.content)
-            except Exception as decode_error:
-                raise HTTPException(
-                    status_code=400,
-                    detail=f"Invalid base64 content for file: {
-                        filePayload.filename}",
-                ) from decode_error
-
-            sanitized_filename = sanitizeFilename(filePayload.filename)
-            tempFilePath = os.path.join(os.getcwd(), sanitized_filename)
-
-            with open(tempFilePath, "wb") as temp_file:
-                temp_file.write(file_content)
-
-            return {"path": tempFilePath, "linkedId": filePayload.linkedId}
-
-        for file in payload.linkedData:
-            tempLinkedDataFiles.append(decodeAndSave(file))
-
-        mongoUri = os.getenv("CONNECTION_STRING")
-        dbName = "alkalyticsDB"
-        migrationService = MigrationService(mongoUri, dbName)
-
-        try:
-            for tempData in tempLinkedDataFiles:
-                dataDf = pd.read_excel(tempData["path"], sheet_name=0)
-                dataDf = migrationService.cleanData(dataDf)
-
-                records = await migrationService.linkData(
-                    dataDf, tempData["linkedId"]
-                )
-
-                if records:
-                    await migrationService.dataSheetsCollection.insert_many(
-                        records
-                    )
-        finally:
-            await migrationService.closeConnection()
-
-        for tempFile in tempLinkedDataFiles:
-            if os.path.exists(tempFile["path"]):
-                os.remove(tempFile["path"])
-
-        return {
-            "status": "success",
-            "message": "Files processed successfully.",
-        }
-
-    except HTTPException as e:
-        raise e
-    except Exception as e:
-        for tempFile in tempLinkedDataFiles:
-            if os.path.exists(tempFile):
-                os.remove(tempFile)
-
-        raise HTTPException(
-            status_code=500, detail=f"Error processing files: {str(e)}"
-        )
-
+    finally:
+            client.close()
 
 class DataRequest(BaseModel):
     experimentId: str
