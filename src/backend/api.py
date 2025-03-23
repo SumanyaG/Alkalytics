155ae27f531e0c4bd0112c6b4c4343d746aad15f
Kate Min <mink9@mcmaster.ca>
2025-03-22 20:29:37 -0400

    feat(backend): add endpoints and calculation functions for efficiency factors

--- src/backend/api.py
+++ src/backend/api.py
@@ -10,9 +10,16 @@ from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
+from datetime import datetime, timedelta
 
 from services.migrationService import MigrationService
 from auth.routes import router as authRouter
+from efficiencies.efficiencyCalculations import (
+    computeCurrentEfficiency,
+    computeOverallEfficiency,
+    computeReactionEfficiency,
+    computeVoltageDropEfficiency
+)
 
 load_dotenv()
 app = FastAPI(docs_url="/docs")
@@ -723,6 +730,166 @@ async def updateColumnTypes(payload: SetColumnTypes):
         raise HTTPException(status_code=500, detail=f"Error updating column types: {str(e)}")
     finally:
         client.close()
+
+
+@app.get("/efficiencies")
+async def getEfficiencies():
+    """
+    Fetches all data from the 'efficiencies' collection.
+    """
+    connection = getConnection("efficiencies")
+    collection, client = connection["collection"], connection["client"]
+
+    try:
+        efficiencies = collection.find()
+        efficenciesList = list(efficiencies)
+        efficenciesList = [cleanData(item) for item in efficenciesList]
+        if efficenciesList:
+            return {"status": "success", "data": efficenciesList}
+        else:
+            raise HTTPException(status_code=404, detail="No efficiency calculations found.")
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Error fetching efficiency calculatiosn: {str(e)}"
+        )
+
+    finally:
+        client.close()
+
+
+async def getExperimentData(experimentId: str, interval: int):
+    """
+    Helper function to fetch experiment data within given time interval.
+    """
+    dataConnection = getConnection("data")
+    dataCollection, dataClient = dataConnection["collection"], dataConnection["client"]
+
+    query = {"experimentId": experimentId}
+    
+    if interval != 0:
+         # get timestamp of first data point to set end time
+        experiment = dataCollection.find_one({ "experimentId": experimentId}, {"_id": 0, "Time": 1})
+        if not experiment:
+            raise HTTPException(status_code=404, detail=f"getExperimentData: No experiment found for experimentId: {experimentId}")
+        startTime = datetime.strptime(experiment["Time"], "%Y/%m/%d %H:%M:%S")
+        endTime = startTime + timedelta(minutes=interval)
+        query["Time"] = {"$lte": endTime.strftime("%Y/%m/%d %H:%M:%S")}
+
+    projection = {"Time": 1, "U Cmm": 1, "U Stac": 1, "I Cmm": 1, "C1 Cond": 1, "C2 Cond": 1}
+
+    try:
+        data = dataCollection.find(query, projection).sort({"Time": 1})
+        if not data:
+            raise HTTPException(status_code=404, detail="No data found for the given experimentId.")
+        datalist = list(data)
+        datalist = [cleanData(item) for item in datalist]
+        return datalist
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error fetching data1: {str(e)}")
+    finally:
+        dataClient.close()
+
+
+async def compute(efficiency, experiment, results, payload):
+    """
+    Helper function to call corresponding efficiency computation functions.
+    """
+    if efficiency in ["Current Efficiency (HCl)", "Current Efficiency (NaOH)"]:
+        compound = "HCL" if efficiency == "Current Efficiency (HCl)" else "NaOH"
+        finalVol, numStacks = experiment.get(compound), experiment.get("# of Stacks")
+        if finalVol is None or numStacks is None:
+            raise Exception(f"Missing data for final volumes or number of stacks (triplets).")
+        return computeCurrentEfficiency(results, compound, finalVol, numStacks)
+
+    if efficiency == "Voltage Drop Efficiency":
+        return computeVoltageDropEfficiency(results)
+
+    if efficiency == "Reaction Efficiency":
+        volHCl, volNaOH = experiment.get("HCL"), experiment.get("NaOH")
+        if volHCl is None or volNaOH is None:
+            raise Exception("Missing data for final volumes.")
+        fullResults = await getExperimentData(payload.experimentId, 0) if payload.timeInterval != 0 else results
+        return computeReactionEfficiency(fullResults, volHCl, volNaOH)
+
+    return None
+
+
+class EfficiencyRequest(BaseModel):
+    experimentId: str
+    selectedEfficiencies: List[str]
+    timeInterval: int
+
+@app.post("/calculate-efficiencies")
+async def calculateEfficiency(payload: EfficiencyRequest):
+    efficienciesConnection = getConnection("efficiencies")
+    efficienciesCollection, efficienciesClient = efficienciesConnection["collection"], efficienciesConnection["client"]
+    
+    # check if calculations were already done before
+    try:
+        existingEntry = efficienciesCollection.find_one({
+            "_id": payload.experimentId + " " + str(payload.timeInterval)
+        })
+        if existingEntry:
+            # check which efficiencies have already been computed
+            computedEfficiencies = {key: value for key, value in existingEntry.items() if key not in ["_id", "experimentId", "Time Interval"]}
+            efficienciesToCompute = [eff for eff in payload.selectedEfficiencies if eff not in computedEfficiencies]
+            if not efficienciesToCompute:
+                return {"message": "Efficiency factors in this request have already been computed", "status": "repeated"}
+        else:
+            computedEfficiencies = {}
+            efficienciesToCompute = payload.selectedEfficiencies
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error fetching data: {str(e)}")
+
+    # fetch experiment metadata
+    expConnection = getConnection("experiments")
+    expCollection, expClient = expConnection["collection"], expConnection["client"]
+    experiment = expCollection.find_one({"experimentId": payload.experimentId})
+    if not experiment:
+        raise HTTPException(status_code=404, detail=f"Experiment metadata not found for {payload.experimentId}.")
+    
+    # fetch detailed experiment data
+    results = await getExperimentData(payload.experimentId, payload.timeInterval)
+
+    for efficiency in efficienciesToCompute:
+        try:
+            computedEfficiencies[efficiency] = await compute(efficiency, experiment, results, payload)
+        except Exception as e:
+            computedEfficiencies[efficiency] = 0
+            print(f"Error computing {efficiency}: {str(e)}")
+            raise Exception(f"Error computing {efficiency}: {str(e)}")
+
+    # handle overall efficiency calculation if requested
+    if "Overall Efficiency" in payload.selectedEfficiencies:
+        values = [v for v in computedEfficiencies.values() if v is not None and v != 0]
+        if values:
+            computedEfficiencies["Overall Efficiency"] = computeOverallEfficiency(values)
+        else:
+            computedEfficiencies["Overall Efficiency"] = 0
+
+    # store computed efficiencies in collections
+    try:
+        efficienciesCollection.update_one(
+            {"_id": payload.experimentId + " " + str(payload.timeInterval),
+             "experimentId": payload.experimentId, 
+             "Time Interval": payload.timeInterval
+            },
+            {"$set": computedEfficiencies},
+            upsert=True
+        )
+        if payload.timeInterval == 0:
+            expCollection.update_one(
+                {"experimentId": payload.experimentId},
+                {"$set": computedEfficiencies}
+            )
+        return {"message": "Efficiency factors computed successfully", "status": "success"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error adding calculations: {str(e)}")
+    finally:
+        expClient.close()
+        efficienciesClient.close()
 
 
 @app.get("/")
---
+++ src/backend/efficiencies/efficiencyCalculations.py
@@ -0,0 +1,126 @@
+import numpy as np
+
+from datetime import datetime
+
+# constants
+FARADAY_CONSTANT = 96485
+MOLAR_MASS = {
+    "HCL": 36.4609,
+    "NaOH": 39.997
+}
+
+# lookup tables to interpolate conductivity -> conc
+COND_TO_CONC = {
+    "conc": [1, 3, 10, 30, 100, 300, 1000, 3000, 10000, 30000, 50000, 100000, 200000],
+    "HCL": [11.7, 35, 116, 340, 1140, 3390, 11100, 32200, 103000, 283000, 432000, 709000, 850000], 
+    "NaOH": [6.2, 18.4, 61.1, 182, 603, 1780, 5820, 16900, 53200, 144000, 223000, 358000, 414000],
+}
+
+def convertCondtoConc(cond: float, compound: str) -> float:
+    """ 
+    Converts a conductivity value to concentration (ppm) using interpolation, then
+    converts ppm to M. 
+    """
+    x_vals = COND_TO_CONC[compound]
+    y_vals = COND_TO_CONC["conc"]
+    ppm = np.interp(cond*1000, x_vals, y_vals)
+    return ppm/(MOLAR_MASS[compound]*1000)
+
+def groupData(data, interval: int = 5) -> list:
+    """Groups data into cumulative 5-minute intervals."""
+    start_time = datetime.strptime(data[0]["Time"], "%Y/%m/%d %H:%M:%S")
+    for entry in data:
+        entry["ElapsedTime"] = (datetime.strptime(entry["Time"], "%Y/%m/%d %H:%M:%S") - start_time).total_seconds() / 60
+
+    cumulative_intervals = []
+    cumulative_data = []
+    for entry in data:
+        cumulative_data.append(entry)
+        if entry["ElapsedTime"] >= len(cumulative_intervals) * interval + interval:
+            cumulative_intervals.append(cumulative_data[:])
+    return cumulative_intervals
+
+def computeCurrentEfficiency(data, compound: str, finalVol: float, numTriplets: int):
+    """ Computes the current efficiency for either HCl or NaOH. """
+    dataField = "C1 Cond" if compound == "HCL" else "C2 Cond"
+    initialConc = convertCondtoConc(data[0][dataField], compound)
+
+    groupedData = groupData(data)
+    interval_effs = []
+
+    for i, group in enumerate(groupedData):
+        timeInterval = (i + 1) * 5
+        if timeInterval == 5:
+            continue
+
+        avgCond = np.mean([entry[dataField] for entry in group])
+        avgCurr = np.mean([entry["I Cmm"] for entry in group])
+
+        if avgCurr == 0:
+            interval_effs.append(0)
+            continue
+
+        avgConc = convertCondtoConc(avgCond, compound)
+        deltaConc = avgConc - initialConc
+        currentEfficiency = (deltaConc * finalVol * FARADAY_CONSTANT) / (numTriplets * timeInterval * avgCurr * 60)
+        interval_effs.append(currentEfficiency * 100)
+
+    return np.mean(interval_effs) if interval_effs else 0
+
+def computeVoltageDropEfficiency(data):
+    """ Computes voltage drop efficiency. """
+    groupedData = groupData(data)
+    interval_effs = []
+
+    for i, group in enumerate(groupedData):
+        timeInterval = (i + 1) * 5
+        if timeInterval == 5:
+            continue
+
+        avgUStack = np.mean([entry["U Stac"] for entry in group])
+        avgUTotal = np.mean([entry["U Cmm"] for entry in group])
+
+        if avgUTotal == 0:
+            interval_effs.append(0)
+            continue
+
+        voltageDropEfficiency = (avgUStack / avgUTotal)
+        interval_effs.append(voltageDropEfficiency * 100)
+
+    return np.mean(interval_effs) if interval_effs else 0
+
+def computeReactionEfficiency(data, volHCl, volNaOH):
+    """ Computes reaction efficiency using final average concentrations and
+    volumes. """
+
+    groupedData = groupData(data)
+    concHCl_avgs = []
+    concNaOH_avgs = []
+
+    for i, group in enumerate(groupedData):
+        timeInterval = (i + 1) * 5
+        if timeInterval == 5:
+            continue
+
+        avgCondHCl = np.mean([entry["C1 Cond"] for entry in group])
+        avgCondNaOH = np.mean([entry["C2 Cond"] for entry in group])
+
+        if avgCondNaOH == 0:
+            concNaOH_avgs.append(0)
+            continue
+
+        avgConcHCl = convertCondtoConc(avgCondHCl, "HCL")
+        avgConcNaOH = convertCondtoConc(avgCondNaOH, "NaOH")
+
+        concHCl_avgs.append(avgConcHCl)
+        concNaOH_avgs.append(avgConcNaOH)
+
+
+    total_hcl_avg =  np.mean(concHCl_avgs)
+    total_naoh_avg = np.mean(concNaOH_avgs)
+    
+    reactionEfficiency = (total_hcl_avg * volHCl) / (total_naoh_avg * volNaOH)
+    return reactionEfficiency * 100
+
+def computeOverallEfficiency(efficiencies):
+    return np.mean(efficiencies) if len(efficiencies) == 4 else None
